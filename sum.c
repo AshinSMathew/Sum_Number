9473e2c110a1acd0ae552c8c77815ccc37e45697
AshinSMathew <145132740+AshinSMathew@users.noreply.github.com>
2024-02-10 08:30:53 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+//sum. c
+#include <stdio.h>
+void main()
+{
+	int a, b, sum;
+	printf("Enter First Number");
+	scanf("%d",&a);
+	printf("Enter Second Number");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("Sum of the given number %d",sum);
+}
